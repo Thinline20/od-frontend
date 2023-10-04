2d3a9fa618589d91636bf1ccf7b4828f12e61c99
pekochan069 <pekochan069@gmail.com>
2023-10-04 23:32:45 +0900

    add room support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "1080p": "1920px",
+      },
+    },
   },
   plugins: [require("daisyui")],
   darkMode: "class",
