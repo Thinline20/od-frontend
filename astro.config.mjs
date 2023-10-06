dbe82ab478c2c29b8a913423ddd94bc6020394a8
pekochan069 <pekochan069@gmail.com>
2023-10-06 20:06:01 +0900

    Make build compatible with django

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "static",
+  compressHTML: process.env.NODE_ENV === "production",
+  build: {
+    format: "file",
+    inlineStylesheets: "auto",
+    assets: "static",
+  },
 });
